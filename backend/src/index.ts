f5e5064e18e62d2302ce7e1328a878ad4012a997
Geek Pastor <chrinovicmukeba123@gmail.com>
2025-12-19 08:17:20 +0200

    change port number for render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,7 +91,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server: Cloud Run/App Hosting fournit PORT
-const port = Number(process.env.PORT) || 8080;
+const port = Number(process.env.PORT) || 50000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`[boot] listening on ${port}`);
